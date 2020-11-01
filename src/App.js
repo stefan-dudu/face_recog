039a0a65b202e2373017128e848d399ec38c4615
Stefan Dudu <dudu.steffan@yahoo.com>
2020-11-01 12:34:43 +0200

    small evolution

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-import { render } from '@testing-library/react';
 
 const particlesConfig = {
 particles: {
@@ -23,6 +22,17 @@ particles: {
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
   render() {
     return (
       <div className="App">
@@ -31,7 +41,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange= {this.onInputChange}/>
         {/*<FaceRecognition /> */}
       </div>
     );
