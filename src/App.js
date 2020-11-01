e7ace675cd61a56d63226ad0bbf0fef29f759929
Stefan Dudu <dudu.steffan@yahoo.com>
2020-11-01 12:13:08 +0200

    working on the face recognition API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+import { render } from '@testing-library/react';
 
 const particlesConfig = {
 particles: {
@@ -21,18 +22,19 @@ particles: {
                 }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params={particlesConfig} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition /> */}
-    </div>
-  );
-}
-
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesConfig} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition /> */}
+      </div>
+    );
+  }
+} 
 export default App;
