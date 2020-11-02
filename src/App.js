d60047996d68a5e99b75a0e60801d9f18cd019e3
Stefan Dudu <dudu.steffan@yahoo.com>
2020-11-02 16:58:25 +0200

    image added, hardcoded

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
@@ -21,6 +23,10 @@ particles: {
  }
 }
 
+const app = new Clarifai.App({
+ apiKey: '1ec4031a38cb43d1bb99d5eed1740991'
+});
+
 class App extends Component {
   constructor() {
     super();
@@ -33,6 +39,22 @@ class App extends Component {
     console.log(event.target.value)
   }
 
+  onButtonSubmit = () => {
+    console.log('click have been pressed');
+    app.models
+      .initModel({
+        id: Clarifai.FACE_DETECT_MODEL,
+      })
+      .then((faceDetectModel) => {
+        return faceDetectModel.predict(
+          "https://media.allure.com/photos/5ea70894ad6aae0008773b2d/1:1/w_1763,h_1763,c_limit/51faces-Social.jpg"
+        );
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  }
+  
   render() {
     return (
       <div className="App">
@@ -41,8 +63,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange= {this.onInputChange}/>
-        {/*<FaceRecognition /> */}
+        <ImageLinkForm 
+          onInputChange= {this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}/>
+        {<FaceRecognition />}
       </div>
     );
   }
