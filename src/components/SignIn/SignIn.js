8c84275a75d9da6b50f0333bb4d93c770dab5424
stefan01-dev <stefan01.dev@gmail.com>
2020-12-13 21:31:28 +0200

    -heroku changes

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -17,7 +17,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signIn', {
+    fetch('https://mysterious-falls-75292.herokuapp.com/signIn', {
       method: 'post',
       headers: {'Content-Type' : 'application/json'},
       body: JSON.stringify({
