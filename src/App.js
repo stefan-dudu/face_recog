620649751d744215323610200073f590b5711772
stefan01-dev <stefan01.dev@gmail.com>
2020-12-16 06:15:49 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ onButtonSubmit = () => {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://mysterious-falls-75292.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
